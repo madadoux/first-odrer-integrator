f1a523bb140c22cf1a60eac711221f402b3eb71a
madadoux <mohedsh@gmail.com>
2016-02-11 03:49:45 +0200

    project
    numerical method to find the first order integration of a given function
    .

---
+++ first odrer integrator/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace first_odrer_integrator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.XF = new System.Windows.Forms.TextBox();
+            this.X0 = new System.Windows.Forms.TextBox();
+            this.Y0 = new System.Windows.Forms.TextBox();
+            this.hVal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbl = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CALC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.XF);
+            this.groupBox1.Controls.Add(this.X0);
+            this.groupBox1.Controls.Add(this.Y0);
+            this.groupBox1.Controls.Add(this.hVal);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(624, 126);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "AVAILABLE TECHS";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // XF
+            // 
+            this.XF.Location = new System.Drawing.Point(534, 99);
+            this.XF.Name = "XF";
+            this.XF.Size = new System.Drawing.Size(31, 20);
+            this.XF.TabIndex = 19;
+            // 
+            // X0
+            // 
+            this.X0.Location = new System.Drawing.Point(534, 63);
+            this.X0.Name = "X0";
+            this.X0.Size = new System.Drawing.Size(31, 20);
+            this.X0.TabIndex = 17;
+            // 
+            // Y0
+            // 
+            this.Y0.Location = new System.Drawing.Point(429, 63);
+            this.Y0.Name = "Y0";
+            this.Y0.Size = new System.Drawing.Size(31, 20);
+            this.Y0.TabIndex = 16;
+            // 
+            // hVal
+            // 
+            this.hVal.Location = new System.Drawing.Point(429, 18);
+            this.hVal.Name = "hVal";
+            this.hVal.Size = new System.Drawing.Size(31, 20);
+            this.hVal.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Lavender;
+            this.label5.Location = new System.Drawing.Point(395, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "when X=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Lavender;
+            this.label4.Location = new System.Drawing.Point(512, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Y\' = 2 (x)(Y)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Lavender;
+            this.label3.Location = new System.Drawing.Point(509, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "X0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lavender;
+            this.label2.Location = new System.Drawing.Point(395, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Y0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lavender;
+            this.label1.Location = new System.Drawing.Point(395, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "h";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(36, 101);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Rung Kutta";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(36, 77);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(51, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Heun";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(36, 53);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(99, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Improved Euler";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(36, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Eueler ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.panel1.Location = new System.Drawing.Point(653, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(387, 271);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // tbl
+            // 
+            this.tbl.AcceptsTab = true;
+            this.tbl.BackColor = System.Drawing.SystemColors.Desktop;
+            this.tbl.ForeColor = System.Drawing.Color.Lime;
+            this.tbl.Location = new System.Drawing.Point(23, 166);
+            this.tbl.Multiline = true;
+            this.tbl.Name = "tbl";
+            this.tbl.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbl.Size = new System.Drawing.Size(624, 458);
+            this.tbl.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 636);
+            this.Controls.Add(this.tbl);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox X0;
+        private System.Windows.Forms.TextBox Y0;
+        private System.Windows.Forms.TextBox hVal;
+        private System.Windows.Forms.TextBox XF;
+        private System.Windows.Forms.TextBox tbl;
+    }
+}
+
---
+++ first odrer integrator/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace first_odrer_integrator
+{
+    public partial class Form1 : Form
+    {
+
+         PointF[] pEuler;
+         PointF[] pHeun;
+         PointF[] pImporvedEuler;
+         PointF[] pRnugKutta4;
+         PointF[] pTrue;
+
+         PointF p1, p2,p3,p4;
+         int size;
+         float H;
+         float Y_0;
+         float X_0;
+         float X_F , Y_F; 
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        float mainF(float x, float y)
+        {
+
+            return (2 * x * y); 
+        }
+
+        void calcTrue()
+        {
+            if (pTrue != null)
+            {
+                pTrue[0].X = X_0; pTrue[0].Y = Y_0;
+
+                for (int i = 1; i < size; i++)
+                {
+                    pTrue[i].X = pTrue[i - 1].X + H;
+                    pTrue[i].Y = (float)Math.Exp(Math.Pow(pTrue[i].X, 2) - 1);
+                }
+            }
+        }
+        void calcEuler()
+        {
+            if (pEuler != null)
+            {
+                pEuler[0].X = X_0; pEuler[0].Y = Y_0;
+
+                for (int i = 1; i < size; i++)
+                {
+                    pEuler[i].X = pEuler[i - 1].X + H;
+                    pEuler[i].Y = pEuler[i - 1].Y + H * mainF(pEuler[i - 1].X, pEuler[i - 1].Y);
+
+                }
+            }
+        }
+        void calcImEU()
+        {
+            if (pImporvedEuler != null)
+            {
+                pImporvedEuler[0].X = X_0; pImporvedEuler[0].Y = Y_0;
+
+                for (int i = 1; i < size; i++)
+                {
+                    pImporvedEuler[i].X =  pImporvedEuler[i - 1].X + H;
+
+                    float tmp0= mainF(pImporvedEuler[i - 1].X, pImporvedEuler  [i - 1].Y);//f(Xi,Yi)
+
+                    float tmp1  = pEuler[i - 1].Y + H * mainF(pImporvedEuler[i - 1].X, pImporvedEuler[i - 1].Y);//Y*i+1
+
+                    float tmp2 = mainF(pImporvedEuler[i].X, tmp1);//f(Xi+1,Y*i+1)
+
+                    pImporvedEuler[i].Y = pImporvedEuler[i - 1].Y + (H / 2) * (tmp0 + tmp2);//Yi+1 = ( Yi + h/2 ( f(Xi,Yi) + f(Xi+1,Y*i+1))
+                }
+            }
+
+
+        }
+        void calcHEUN()
+        {
+            if (pHeun != null)
+            {
+                pHeun[0].X = X_0; pHeun[0].Y = Y_0;
+
+                for (int i = 1; i < size; i++)
+                {
+
+                     pHeun[i].X = pHeun[i - 1].X + H;
+
+
+                    float k1 = mainF(pHeun[i - 1].X, pHeun[i - 1].Y);
+                    float k2 = mainF(pHeun[i - 1].X + H, pHeun[i - 1].Y + k1 * H);
+
+                    pHeun[i].Y = pHeun[i - 1].Y + H*(k1 / 2+ k2 / 2); 
+
+
+                }
+            }
+
+        }
+        void calcRungKutta()
+        {
+
+            if (pRnugKutta4 != null)
+            {
+
+                pRnugKutta4[0].X = X_0; pRnugKutta4[0].Y = Y_0;
+
+                for (int i = 1; i <size  ; i++)
+                {
+                    pRnugKutta4[i].X = pRnugKutta4[i - 1].X + H;
+
+                    float k1 = mainF(pRnugKutta4[i - 1].X, pRnugKutta4[i - 1].Y);
+                    float k2 = mainF(pRnugKutta4[i - 1].X +( H / 2), pRnugKutta4[i - 1].Y + (k1 * H) / 2);
+                    float k3 = mainF(pRnugKutta4[i - 1].X +( H / 2), pRnugKutta4[i - 1].Y + (k2 * H) / 2);
+                    float k4 = mainF(pRnugKutta4[i - 1].X + H, pRnugKutta4[i - 1].Y + (k3 * H));
+
+                    pRnugKutta4[i].Y = pRnugKutta4[i - 1].Y + (H/6)* (k1 + 2 * k2 + 2 * k3 + k4);  
+
+
+                }
+
+            }
+
+        }
+
+        void VarInit()
+        {
+
+            float.TryParse(Y0.Text, out Y_0);
+            float.TryParse(X0.Text, out X_0);
+            float.TryParse(hVal.Text, out H);
+
+            float.TryParse(XF.Text, out X_F);
+
+            if (H != 0)
+            {
+                size = (int)((X_F - X_0) / H) + 1;
+                if (size <= 0)
+                { MessageBox.Show("X_F can't be less than X_0 or h ! enter  the X_F again "); 
+                }
+            }
+            else
+            {
+                MessageBox.Show("H can't be 0 ");
+            }
+      
+        }
+        void DisTable()
+        {
+            tbl.Text = "X\r\nYtrue\r\n[1]nYEuler|E|RE\r\n[2]YImprovedEuler|E|RE\r\n[3]YHeun|E|RE\r\n[4]YRungKutta|E|RE\r\n"+"___________________\r\n";
+            for (int i = 0; i < size; i++)
+            {
+                tbl.Text += pTrue[i].X.ToString() + "\r\n" + Math.Round(pTrue[i].Y, 2).ToString()+ "\r\n"
+                +"[1]"+
+                ((pEuler != null) ? (Math.Round(pEuler[i].Y, 2).ToString()) + '|' + Err(pTrue[i].Y, pEuler[i].Y).ToString() + '|' + RE(pTrue[i].Y, pEuler[i].Y).ToString() : "--") + "\r\n"
+                +"[2]"+
+               ((pImporvedEuler != null) ? (Math.Round(pImporvedEuler[i].Y, 2).ToString()) + '|' + Err(pTrue[i].Y, pImporvedEuler[i].Y).ToString() + '|' + RE(pImporvedEuler[i].Y, pImporvedEuler[i].Y).ToString() : "--") + "\r\n"
+                +"[3]"+
+               ((pHeun != null) ? (Math.Round(pHeun[i].Y, 2).ToString()) + '|' + Err(pTrue[i].Y, pHeun[i].Y).ToString() + '|' + RE(pTrue[i].Y, pHeun[i].Y).ToString() : "--") + "\r\n"
+                +"[4]"+
+               ((pRnugKutta4 != null) ? (Math.Round(pRnugKutta4[i].Y, 2).ToString()) + '|' + Err(pRnugKutta4[i].Y, pRnugKutta4[i].Y).ToString() + '|' + RE(pRnugKutta4[i].Y, pRnugKutta4[i].Y).ToString() : "--") + "\r\n"+
+              
+              "___________________\r\n";
+
+            }
+        }
+
+        double Err( double v1 , double  v2 ){
+            return  Math.Abs(v2 - v1); 
+        
+        }
+
+        double RE ( double v1, double v2 ){
+
+            return Math.Abs((v1 - v2) / v1) * 100;
+        }
+
+        private void Calc()
+        {
+            VarInit();
+            if (size > 0)
+            {
+                pTrue = new PointF[size];
+                calcTrue();
+
+
+                if (checkBox1.Checked)
+                {
+                    pEuler = new PointF[size];
+                    calcEuler();
+                }
+                if (checkBox2.Checked)
+                {
+                    pImporvedEuler = new PointF[size];
+                    calcImEU(); 
+                }
+                if (checkBox3.Checked)
+                {
+                    pHeun = new PointF[size];
+                    calcHEUN(); 
+                }
+                if (checkBox4.Checked)
+                {
+                    pRnugKutta4 = new PointF[size];
+                    calcRungKutta();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        public void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            
+            p1 = new PointF(150, 0);
+            p2 = new PointF(150, panel1.Size.Height);
+            p3 = new PointF(0, panel1.Size.Height - 30);
+            p4 = new PointF(panel1.Size.Width, panel1.Size.Height - 30);
+
+            e.Graphics.DrawString("TRUE", DefaultFont, Brushes.Black, 0f, 0f);
+            e.Graphics.DrawString("EULER", DefaultFont, Brushes.Blue, 0f, 15f);
+            e.Graphics.DrawString("IMPROVED_EULER", DefaultFont, Brushes.Red, 0f,  30f);
+            e.Graphics.DrawString("HEUN", DefaultFont, Brushes.Aqua, 0f, 45f);
+            e.Graphics.DrawString("RUNG_KUTTA", DefaultFont, Brushes.Purple, 0f, 60f);
+
+            e.Graphics.DrawLine(Pens.Gold, p1, p2);
+            e.Graphics.DrawLine(Pens.Gold, p3, p4); 
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            e.Graphics.TranslateTransform(150 ,panel1.Size.Height - 30);
+            e.Graphics.ScaleTransform(40,-.025f);
+
+
+            if (pTrue != null)
+            {
+                try
+                {
+                    e.Graphics.DrawLines(Pens.Black, pTrue.ToList().Where(p => p.Y <= int.MaxValue).ToArray());
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("CAN'T Draw (true) such these BIG numbers TRY to lower the |h| or |X| that u want ");
+                    //throw;
+                }
+            }
+
+
+            if (pEuler != null)
+            {
+                try
+                {
+                    e.Graphics.DrawLines(Pens.Blue, pEuler.ToList().Where(p => p.Y <= int.MaxValue).ToArray());
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("CAN'T Draw (Euler) such these BIG numbers TRY to lower the |h| or |X| that u want ");                   
+                    //throw;
+                }
+            }
+
+
+            if (pImporvedEuler != null)
+            {
+                try
+                {
+                    e.Graphics.DrawLines(Pens.Red,  pImporvedEuler.ToList().Where(p => p.Y <= int.MaxValue).ToArray());
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("CAN'T Draw (ImprovedEuler) such these BIG numbers TRY to lower the |h| or |X| that u want ");
+                    //throw;
+                }
+            }
+
+            if (pHeun != null)
+            {
+                try
+                {
+                    e.Graphics.DrawLines(Pens.Aqua, pHeun.ToList().Where(p => p.Y <= int.MaxValue).ToArray());
+                }
+                catch (Exception)
+                {
+                   MessageBox.Show("CAN'T Draw  (HEUN) such these BIG numbers TRY to lower the |h| or |X| that u want ");
+                    //throw;
+                }
+            }
+
+            if (pRnugKutta4 != null)
+            {
+                try
+                {
+                    e.Graphics.DrawLines(Pens.Purple, pRnugKutta4.ToList().Where(p => p.Y <= int.MaxValue).ToArray());
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("CAN'T Draw  (RungKutta) such these BIG numbers TRY to lower the |h| or |X| that u want ");
+                    //throw;
+                }
+            }
+
+
+        }
+            
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Calc(); 
+            panel1.Refresh();
+            DisTable(); 
+           
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             pEuler=null;
+             pHeun=null;
+             pImporvedEuler=null;
+             pRnugKutta4=null;
+             pTrue=null;
+
+            tbl.Text = "Cleared ";
+            panel1.Refresh();
+        }
+
+    }
+}
